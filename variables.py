f3d0d532471f38e9623a76e4773aba1b6f0e01e1
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-05-01 18:50:58 -0500

    Variables and expressions

---
+++ variables.py
@@ -0,0 +1,23 @@
+#
+# Example file for variables
+#
+
+# Declare a variable and initialize it
+f: int = 0
+print(f)
+
+# Re-declaring the variable works
+f: str = 'abc'
+print(f)
+
+# ERROR: variables of different types cannot be combined
+print('This is a string ' + str(123))
+
+# Global vs local variables in functions
+def someFunction():
+    global f
+    f = 'def'
+    print(f)
+
+del f
+print(f)
