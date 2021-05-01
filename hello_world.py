a62d70481646d8c54d2047a663be52cdcda0b1d2
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-05-01 18:37:41 -0500

    Debugging hello world script

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#
+# Example file for HelloWorld
+#
+
+
+def main() -> None:
+    print('Hello world')
+    name = input('What is your name? ')
+    print('Nice to meet you,', name)
+
+if __name__ == '__main__':
+    main()
