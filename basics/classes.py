77bb96f7442cc71fd6321f2c2232dbdec995e46d
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-05-01 19:59:15 -0500

    Classes

---
+++ basics/classes.py
@@ -0,0 +1,36 @@
+#
+# Example file for working with classes
+#
+
+
+class MyClass():
+
+    def method1(self) -> None:
+        print('MyClass method1')
+
+    def method2(self, some_string: str) -> None:
+        print(f'MyClass method2: {some_string}')
+
+
+class AnotherClass(MyClass):
+
+    def method1(self) -> None:
+        MyClass.method1(self)
+        print('AnotherClass method1')
+
+    def method2(self, some_string: str) -> None:
+        print(f'AnotherClass method2')
+
+
+def main() -> None:
+    c1 = MyClass()
+    c1.method1()
+    c1.method2('This is a string')
+
+    c2 = AnotherClass()
+    c2.method1()
+    c2.method2('This is a string')
+
+
+if __name__ == '__main__':
+    main()
