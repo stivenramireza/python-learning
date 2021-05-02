1d099480685bea096b04a491c3cea03fd09671d7
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-05-01 20:12:17 -0500

    Modules

---
+++ basics/modules.py
@@ -0,0 +1,11 @@
+# Import the math module, which contains features for working with math
+import math
+
+# The math module contains lots of pre-built functions
+print('The square root of 16 is', math.sqrt(16))
+
+# In addition to functions, some modules contain useful constants
+print('Pi is:', math.pi)
+
+# Try some of the math functions for yourself here:
+print('The cos of 60.5 is', math.cos(60.5))
