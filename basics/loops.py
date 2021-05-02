e406accd16aa76c6d25517e5c3c970b501cc7226
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-05-01 19:47:02 -0500

    Loops

---
+++ basics/loops.py
@@ -0,0 +1,36 @@
+#
+# Example file for working with loops
+#
+
+
+def main() -> None:
+    x: int = 0
+
+    # Define a while loop
+    while x < 5:
+        print(x)
+        x = x + 1
+
+    # Define a for loop
+    for x in range(5, 10):
+        print(x)
+    
+    # Use a loop over a collection
+    days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    for d in days:
+        print(d)
+
+    # Use the break and continue statements
+    for x in range(5, 10):
+        # if x == 7: break
+        if x % 2 == 0 : continue
+        print(x)
+
+    # Using the enumerate() function to get index
+    days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    for i, d in enumerate(days):
+        print(i, d)
+
+
+if __name__ == '__main__':
+    main()
