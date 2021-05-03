104d74fb03d7e195531cb2d759f49b619eecdf40
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-05-02 21:18:56 -0500

    HTML parsing

--- web_data/html_parsing.py
+++ web_data/html_parsing.py
@@ -25,24 +25,25 @@ class MyHTMLParser(HTMLParser):
       for a in attrs:
         print("\t", a[0],"=",a[1])
       
-  # function to handle the ending tag
+  # Function to handle the ending tag
   def handle_endtag(self, tag: str) -> None:
     print("Encountered an end tag:", tag)
     pos = self.getpos()
     print("\tAt line: ", pos[0], " position ", pos[1])
     
-  # function to handle character and text data (tag contents)
+  # Function to handle character and text data (tag contents)
   def handle_data(self, data: str) -> None:
     if (data.isspace()): return
     print("Encountered some text data:", data)
     pos = self.getpos()
     print("\tAt line: ", pos[0], " position ", pos[1])
   
-  # function to handle the processing of HTML comments
+  # Function to handle the processing of HTML comments
   def handle_comment(self, data: str) -> None:
     print("Encountered comment:", data)
     pos = self.getpos()
     print("\tAt line: ", pos[0], " position ", pos[1])
+
 
 def main() -> None:
   # Instantiate the parser and feed it some HTML
