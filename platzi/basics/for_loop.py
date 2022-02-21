31ed3e819eb8f5846182fbfcb2eaa879e8e894ef
Stiven Ramírez Arango <sramirez@superfuds.com.co>
2022-02-20 19:00:32 -0500

    while and for loops

---
+++ platzi/basics/for_loop.py
@@ -0,0 +1,20 @@
+def main() -> None:
+    # counter = 1
+    # print(counter)
+
+    # while counter < 1000:
+    #     counter += 1
+    #     print(counter)
+
+    # a = list(range(1000))
+    # print(a)
+
+    for counter in range(1, 1001):
+        print(counter)
+
+    for i in range(0, 10):
+        print(f"11*{i} = {11 * i}")
+
+
+if __name__ == "__main__":
+    main()
