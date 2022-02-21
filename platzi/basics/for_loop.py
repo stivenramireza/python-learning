158cbc951db3e944b43e9677aa21811eae8e1711
Stiven Ramírez Arango <sramirez@superfuds.com.co>
2022-02-20 19:10:38 -0500

    Loops

--- platzi/basics/for_loop.py
+++ platzi/basics/for_loop.py
@@ -1,19 +1,39 @@
+def loop_a_name(name: str) -> None:
+    for letter in name:
+        print(letter)
+
+
+def loop_a_phrase(phrase: str) -> None:
+    for character in phrase:
+        print(character.upper())
+
+
 def main() -> None:
-    # counter = 1
-    # print(counter)
+    # Loop a counter until 1000
 
-    # while counter < 1000:
-    #     counter += 1
-    #     print(counter)
+    # Using while
+    counter = 1
+    print(counter)
 
-    # a = list(range(1000))
-    # print(a)
+    while counter < 1000:
+        counter += 1
+        print(counter)
 
+    # Using for
     for counter in range(1, 1001):
         print(counter)
 
+    # Loop 11 table
     for i in range(0, 10):
         print(f"11*{i} = {11 * i}")
+
+    # Loop a name
+    name = input("Enter your name: ")
+    loop_a_name(name)
+
+    # Loop a phrase with upper()
+    phrase = input("Enter a phrase: ")
+    loop_a_phrase(phrase)
 
 
 if __name__ == "__main__":
