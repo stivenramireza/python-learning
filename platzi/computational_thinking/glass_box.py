47b7717b5b98b1cbf14f47422e19632ad5e39133
Stiven Ramírez Arango <sramirez@superfuds.com.co>
2022-03-13 21:30:30 -0500

    Regression tests

---
+++ platzi/computational_thinking/glass_box.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+def is_years_older(age: int) -> bool:
+    return True if age >= 18 else False
+
+
+class GlassBoxTest(unittest.TestCase):
+    
+    def test_is_of_legal_age(self) -> None:
+        age = 20
+
+        result = is_years_older(age)
+
+        self.assertEqual(result, True)
+
+    def test_is_minor_age(self) -> None:
+        age = 15
+
+        result = is_years_older(age)
+
+        self.assertEqual(result, False)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
