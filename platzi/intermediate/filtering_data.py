0341cc84f2f1c9e2d7a8d4b11ca718d822d8ab91
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-03-28 15:38:25 -0500

    Filtering data

--- platzi/intermediate/filtering_data.py
+++ platzi/intermediate/filtering_data.py
@@ -73,8 +73,12 @@ DATA = [
 
 
 def main():
-    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
-    all_platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    all_python_devs = [
+        worker["name"] for worker in DATA if worker["language"] == "python"
+    ]
+    all_platzi_workers = [
+        worker["name"] for worker in DATA if worker["organization"] == "Platzi"
+    ]
     adults = list(filter(lambda worker: worker["age"] > 18, DATA))
     adults_with_name = list(map(lambda worker: worker["name"], adults))
     old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
