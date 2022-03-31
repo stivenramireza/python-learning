8f857b3418bf8ec8b5c7bc6742265f3e55217d82
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-03-31 16:45:56 -0500

    Sets

---
+++ platzi/advanced/sets.py
@@ -0,0 +1,15 @@
+# [1, 1, 2, 2, 4] -> [1, 2, 4]
+
+
+def remove_duplicates(some_list: list[int]) -> None:
+    return list(set(some_list))
+
+
+def main() -> None:
+    random_list = [1, 1, 2, 2, 4]
+    without_duplicates = remove_duplicates(random_list)
+    print(without_duplicates)
+
+
+if __name__ == "__main__":
+    main()
