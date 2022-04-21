0d45bf8c3f2bb200d7bda31f06422c481cb605d3
Stiven Ramírez Arango <stivenramirez@habi.co>
2022-04-20 21:35:07 -0500

    Functions

--- platzi/crud/main.py
+++ platzi/crud/main.py
@@ -1,7 +1,29 @@
-def main() -> None:
-    clients = "pablo,ricardo,"
-    clients += "david"
+from venv import create
+
+
+clients = "Pablo,Ricardo,"
+
+
+def create_client(client_name: str) -> None:
+    global clients
+    clients += client_name
+    _add_comma()
+
+
+def get_clients() -> None:
+    global clients
     print(clients)
+
+
+def _add_comma() -> None:
+    global clients
+    clients += ","
+
+
+def main() -> None:
+    get_clients()
+    create_client("David")
+    get_clients()
 
 
 if __name__ == "__main__":
