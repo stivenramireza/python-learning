a7300eed52d052dff954202cf33ba35a43369789
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-05-22 18:53:11 -0500

    Lists

--- platzi/crud/main.py
+++ platzi/crud/main.py
@@ -1,30 +1,30 @@
 import sys
 
-clients = "Pablo,Ricardo,"
+clients = ['pablo', 'ricardo']
 
-CLIENT_NO_EXISTS = "Client is not in the client's list"
+CLIENT_NO_EXISTS = 'Client is not in the client\'s list'
 
 
 def create_client(client_name: str) -> None:
     global clients
 
     if client_name not in clients:
-        clients += client_name
-        _add_comma()
+        clients.append(client_name)
     else:
-        print("Client already is in the client's name")
+        print('Client already is in the client\'s name')
 
 
 def list_clients() -> None:
-    global clients
-    print(clients)
+    for idx, client in enumerate(clients):
+        print('{}: {}'.format(idx, client))
 
 
 def update_client(client_name: str, updated_client_name: str) -> None:
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name, updated_client_name)
+        index = clients.index(client_name)
+        clients[index] = updated_client_name
     else:
         print(CLIENT_NO_EXISTS)
 
@@ -33,29 +33,22 @@ def delete_client(client_name: str) -> None:
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name, "")
+        clients.remove(client_name)
     else:
         print(CLIENT_NO_EXISTS)
 
 
 def search_client(client_name: str) -> bool:
-    clients_list = clients.split(",")
-
-    for client in clients_list:
+    for client in clients:
         if client != client_name:
             continue
         else:
             return True
 
 
-def _add_comma() -> None:
-    global clients
-    clients += ","
-
-
 def _print_welcome() -> None:
     print(
-        """
+        '''
     WELCOME TO STIVENRAMIREZA SALES
 
     What would you like to do today?
@@ -65,7 +58,7 @@ def _print_welcome() -> None:
     - [U]pdate client
     - [D]elete client
     - [S]earch client
-    """
+    '''
     )
 
 
@@ -73,9 +66,9 @@ def _get_client_name() -> str:
     client_name = None
 
     while not client_name:
-        client_name = input("What is the client name? ")
+        client_name = input('What is the client name? ')
 
-        if client_name == "exit":
+        if client_name == 'exit':
             client_name = None
             break
 
@@ -92,32 +85,36 @@ def main() -> None:
     command = command.upper()
 
     match command:
-        case "C":
+        case 'C':
             client_name = _get_client_name()
             create_client(client_name)
             list_clients()
-        case "L":
+        case 'L':
             list_clients()
-        case "D":
+        case 'D':
             client_name = _get_client_name()
             delete_client(client_name)
             list_clients()
-        case "U":
+        case 'U':
             client_name = _get_client_name()
-            updated_client_name = input("What is the updated client name? ")
+            updated_client_name = input('What is the updated client name? ')
             update_client(client_name, updated_client_name)
             list_clients()
-        case "S":
+        case 'S':
             client_name = _get_client_name()
             is_client_found = search_client(client_name)
 
             if is_client_found:
-                print("The client is in the client's list")
+                print('The client is in the client\'s list')
             else:
-                print(f"The client {client_name} is not in the client's list")
+                print(
+                    'The client {} is not in the client\'s list'.format(
+                        client_name
+                    )
+                )
         case _:
-            print("Invalid command")
+            print('Invalid command')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
