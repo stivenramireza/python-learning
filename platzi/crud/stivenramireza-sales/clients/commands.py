e33462dd2f892b9d27398b4ee24447d6b635d535
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-05-27 12:02:16 -0500

    Update command

--- platzi/crud/stivenramireza-sales/clients/commands.py
+++ platzi/crud/stivenramireza-sales/clients/commands.py
@@ -53,10 +53,36 @@ def list(ctx: object) -> None:
 
 
 @clients.command()
+@click.argument('client_uid', type=str)
 @click.pass_context
-def update(ctx: object, client_uid: int) -> None:
+def update(ctx: object, client_uid: str) -> None:
     """Updates a client"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    clients = client_service.list_clients()
+    client = [client for client in clients if client['uid'] == client_uid]
+
+    if client:
+        client = _update_client_flow(Client(**client[0]))
+        client_service.update_client(client)
+        click.echo('Client updated')
+    else:
+        click.echo('Client not found')
+
+
+def _update_client_flow(client: Client) -> bool:
+    click.echo('Leave empty if you don\'t want to modify the value')
+
+    client.name = click.prompt('New name: ', type=str, default=client.name)
+    client.company = click.prompt(
+        'New company: ', type=str, default=client.company
+    )
+    client.email = click.prompt('New email: ', type=str, default=client.email)
+    client.position = click.prompt(
+        'New position: ', type=str, default=client.position
+    )
+
+    return client
 
 
 @clients.command()
--- platzi/crud/stivenramireza-sales/clients/services.py
+++ platzi/crud/stivenramireza-sales/clients/services.py
@@ -1,4 +1,6 @@
+import os
 import csv
+from typing import List, Dict
 
 from clients.models import Client
 
@@ -16,3 +18,27 @@ class ClientService:
         with open(self.table_name, mode='r') as f:
             reader = csv.DictReader(f, fieldnames=Client.schema())
             return list(reader)
+
+    def update_client(self, updated_client: Client) -> None:
+        clients = self.list_clients()
+
+        updated_clients = list(
+            map(
+                lambda c: updated_client.to_dict()
+                if c.get('uid') == updated_client.uid
+                else c,
+                clients,
+            )
+        )
+
+        self._save_to_disk(updated_clients)
+
+    def _save_to_disk(self, clients: List[Dict[str, any]]) -> None:
+        tmp_table_name = self.table_name + '.tmp'
+
+        with open(tmp_table_name, mode='w') as f:
+            writer = csv.DictWriter(f, fieldnames=Client.schema())
+            writer.writerows(clients)
+
+        os.remove(self.table_name)
+        os.rename(tmp_table_name, self.table_name)
